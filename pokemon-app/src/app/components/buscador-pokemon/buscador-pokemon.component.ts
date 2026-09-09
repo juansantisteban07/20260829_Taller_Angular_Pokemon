420c0a0b7c08ca422b8353006b0c397e7b369df7
Juan Sebastian <jdiazsantisteban77@gmail.com>
2026-09-09 01:17:57 +0000

    creacion de pagina de buscador de pokemon

--- pokemon-app/src/app/components/buscador-pokemon/buscador-pokemon.component.ts
+++ pokemon-app/src/app/components/buscador-pokemon/buscador-pokemon.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export interface pokemonData {
+  name: string;
+  image: string;
+  type: string;
+}
 
 @Component({
   selector: 'app-buscador-pokemon',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './buscador-pokemon.component.html',
   styleUrl: './buscador-pokemon.component.css'
 })
-export class BuscadorPokemonComponent {
 
+export class BuscadorPokemonComponent {
+  nombrePokemoninput = signal('');
+  pokemon = signal<pokemonData | null>(null);
+  mensajeError = signal<string | null>(null);
+
+  async buscarPokemon() {
+    const nombrePokemon = this.nombrePokemoninput().trim().toLowerCase();
+
+    if(!nombrePokemon) return;
+
+    this.mensajeError.set(null);
+
+    try {
+
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
+
+
+    if (!respuesta.ok) {
+      throw new Error('No se encontró el Pokémon');
+    }
+
+    const datos = await respuesta.json();
+
+    this.pokemon.set({
+      name: datos.name.toUpperCase(),
+      image: datos.sprites.front_default,
+      type: datos.types.map((tipo: any) => tipo.type.name).join(', ')
+    });
+  
+    }catch (error:any) {
+      this.pokemon.set(null);
+      this.mensajeError.set(error.message);
+      }
+}
 }
--- pokemon-app/src/app/components/registro-usuario/registro-usuario.component.ts
+++ pokemon-app/src/app/components/registro-usuario/registro-usuario.component.ts
@@ -2,19 +2,17 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 export interface Usuario {
-    id : number;
-    nombreCompleto : string;
-    documento : { 
-      tipo: string;
-      numero: string
-    };
-    fechaNacimiento : string;
-    correo : string;
-    datosPersonales : boolean;  
-    fechaRegistro: string;
-
+  id: number;
+  nombreCompleto: string;
+  documento: {
+    tipo: string;
+    numero: string;
+  };
+  fechaNacimiento: string;
+  correo: string;
+  datosPersonales: boolean;
+  fechaRegistro: string;
 }
-
 
 @Component({
   imports: [FormsModule],
@@ -26,36 +24,34 @@ export interface Usuario {
 export class RegistroUsuarioComponent {
   nombre = signal('');
   apellido = signal('');
-  tipo_doc = signal('CC');
+  tipo_doc = signal('');
   dni = signal('');
   fecha_nacimiento = signal('');
   correo = signal('');
   datos_personales = signal(false);
 
-    ultimoUsuario = signal<Usuario | null>(null);
+  ultimoUsuario = signal<Usuario | null>(null);
 
-    guardarUsuario(): void {
+ guardarUsuario() {
         if(!this.datos_personales()){
           alert('Debes aceptar el tratamiento de datos personales');
           return;
         }
 
-        const usuarioCreado: Usuario = {
-        id : Date.now(),
-        nombreCompleto : `${this.nombre()} ${this.apellido()}`,
-        documento : {
-          tipo: this.tipo_doc(),
-          numero: this.dni()
-        },
-        fechaNacimiento : this.fecha_nacimiento(),
-        correo : this.correo(),
-        datosPersonales : this.datos_personales(),  
-        fechaRegistro: new Date().toLocaleDateString()
-    }
+    const usuarioCreado: Usuario = {
+      id: Date.now(),
+      nombreCompleto: `${this.nombre()} ${this.apellido()}`,
+      documento: {
+        tipo: this.tipo_doc(),
+        numero: this.dni(),
+      },
+      fechaNacimiento: this.fecha_nacimiento(),
+      correo: this.correo(),
+      datosPersonales: this.datos_personales(),
+      fechaRegistro: new Date().toLocaleDateString(),
+    };
 
-    localStorage.setItem(`usuarioCreado${usuarioCreado.id}`, JSON.stringify(usuarioCreado));
-
+    localStorage.setItem(usuarioCreado.id.toString(), JSON.stringify(usuarioCreado));
     this.ultimoUsuario.set(usuarioCreado);
-
-    }
   }
+}
