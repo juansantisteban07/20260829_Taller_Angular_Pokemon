67f933b62816f53f3127804f180b9dd882380f93
Juan Sebastian <jdiazsantisteban77@gmail.com>
2026-09-10 02:04:27 +0000

    Creacion de directivas y servicios

--- pokemon-app/src/app/services/pokemon-storage.service.ts
+++ pokemon-app/src/app/services/pokemon-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject, signal } from '@angular/core';
-import { httpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 export interface PokemonTarjeta {
   id: number;
@@ -15,7 +15,7 @@ export interface PokemonTarjeta {
   providedIn: 'root'
 })
 export class PokemonStorageService {
-  private http = inject(httpClient);
+  private http = inject(HttpClient);
   private readonly storageKey = 'equipo_pokemon_registrado';
 
   misPokemons = signal<PokemonTarjeta[]>([]);
@@ -29,5 +29,39 @@ export class PokemonStorageService {
     if (data) {
       this.misPokemons.set(JSON.parse(data));
     }
+  }
+
+  //1. OBTENER DATOS DE LA API
+  buscarEnAPI(nombreOId: string){
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${nombreOId.toLowerCase()}`)
+  }
+
+  //2. guardar/crear nuevo pokemon dentro de el maleto.
+
+  guardarPokemon(nuevo: PokemonTarjeta){
+    const actualizados = [...this.misPokemons(), nuevo];
+    this.misPokemons.set(actualizados);
+    localStorage.setItem(this.storageKey, JSON.stringify(actualizados));
+  };
+
+
+  //3. Actualizar pokemon favorito.
+  actualizarFavorito(id : number){
+    const actualizados = this.misPokemons().map(poke => {
+      if(poke.id === id){
+        return { ...poke, esFavorito: !poke.esFavorito };
+      }
+      return poke;
+    });
+  this.misPokemons.set(actualizados)
+  localStorage.setItem(this.storageKey, JSON.stringify(actualizados));
+}
+
+  //4. Eliminar pokemon del maleto
+  eliminarPokemon(id: number){
+    const filtrados = this.misPokemons().filter(poke => poke.id !== id);
+    this.misPokemons.set(filtrados);
+     localStorage.setItem(this.storageKey, JSON.stringify(filtrados));
+  }
 
 }
